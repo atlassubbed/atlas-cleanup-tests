79874bc2fa61ad814e5d861ee546b1bc7d709db1
atlassubbed <atlassubbed@gmail.com>
2018-06-24 22:53:52 -0400

    exports a factory which creates a cleanup wrapper for mocha tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello from atlas-cleanup-tests!")
+const run = (test, cleanup, done) => {
+  let error, isDone = false;
+  try { 
+    test(err => {
+      isDone = true, cleanup(), done(err);
+    }) 
+  }
+  catch (err) { error = err }
+  finally {
+    if (!isDone){
+      cleanup();
+      if (error) throw error;
+    }
+  }
+}
+
+module.exports = cleanup => test => {
+  // returns a mocha test
+  return test.length ?
+    function(done){run(test, cleanup, done)} : 
+    function(){run(test, cleanup)}
+}
